7164120b1e2b26410548aeea7716569eaabe9625
Doidjin <LeonardoCabbau@MacBook-Air-de-LEONARDO.local>
2017-07-11 19:03:15 -0300

    Simplificando requisicoes Ajax com Fetch API

--- javascript-avancado-i-aula1/aluraframe/client/js/app/services/HttpService.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/services/HttpService.js
@@ -1,21 +1,24 @@
 class HttpService {
 
+    _handleErrors(res){
+        if(!res.ok) throw new Error(res.statusText);
+        return res;
+    }
+
     get(url){
 
-        return new Promise((resolve, reject) => {
-            let xhr = new XMLHttpRequest();
+        return fetch(url)
+            .then(res => this._handleErrors(res))
+            .then(res => res.json());
+    }
 
-            xhr.open('GET', url);
-            xhr.onreadystatechange = () => {
-                if(xhr.readyState == 4) {
-                    if(xhr.status == 200) {
-                        resolve(JSON.parse(xhr.responseText));
-                    } else {
-                        reject(xhr.responseText);
-                    }  
-                }
-            }
-            xhr.send();
-        });
+    post(url, dado){
+
+        return fetch(url, {
+            headers: { 'Content-type': 'application/json'},
+            method: 'post',
+            body: JSON.stringify(dado)
+        })
+        .then(res => this._handleErrors(res));
     }
 }
